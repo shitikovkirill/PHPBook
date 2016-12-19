013e1327d2a8c088eb2f2594b5a0983eb3c44f10
shitikovkirill <sh.kiruh@gmail.com>
2016-12-19 15:25:52 +0200

    add array stack

---
+++ zend_book/array/SetFunctionalityTest.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: kirill
+ * Date: 19.12.16
+ * Time: 15:11
+ */
+class SetFunctionalityTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function array_diff()
+    {
+        // array_diff_assoc()
+        // array_diff_uassoc()
+        // array_diff_key()
+        // array_diff_ukey()
+
+        $a = array (1, 2, 3, 4);
+        $b = array (1, 3, 4, 5, 6);
+        $diff = array_diff($a, $b);
+        //print_r($diff);
+        $this->assertEquals('2', implode(array_values($diff)));
+    }
+
+    public function array_intersect()
+    {
+        // array_intersect_key()
+        // array_intersect_ukey()
+        // array_intersect_assoc()
+        // array_intersect_uassoc()
+
+        $a = array (1, 2, 3);
+        $b = array (1, 3, 4);
+        $inter = array_intersect($a, $b);
+        print_r($inter);
+        $this->assertEquals('1, 3', implode(', ', array_values($inter)));
+    }
+}
---
+++ zend_book/array/StacksTest.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: kirill
+ * Date: 19.12.16
+ * Time: 14:44
+ */
+class StacksTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function stacks_posledniy_zashel_perviy_vishel()
+    {
+        $stack = array();
+        array_push($stack, 'bar', 'other', 'baz');
+        //print_r($stack);
+        $this->assertCount(3, $stack);
+
+        $last_in = array_pop($stack);
+        $this->assertCount(2, $stack);
+        //print_r($last_in);
+        $this->assertEquals('baz', $last_in);
+
+        $stack = array_flip($stack);
+
+        $this->assertArrayHasKey('bar', $stack);
+        $this->assertArrayNotHasKey('baz', $stack);
+    }
+
+    /**
+     * @test
+     */
+    public function stacks()
+    {
+        $stack = array('qux', 'bar', 'baz');
+        $first_element = array_shift($stack);
+        $this->assertCount(2, $stack);
+        $this->assertEquals('qux', $first_element);
+        //print_r($stack);
+
+        array_unshift($stack, 'foo');
+        $this->assertCount(3, $stack);
+        $this->assertEquals('foo', $stack[0]);
+        //print_r($stack);
+    }
+}
