ad8afdf6ea28dd17746fbad12523f04a196ddd52
shitikovkirill <sh.kiruh@gmail.com>
2016-12-29 15:44:38 +0200

    regular expressions

---
+++ zend_book/formatting/FormatStringsTest.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: kirill
+ * Date: 29.12.16
+ * Time: 13:18
+ */
+class FormatStringsTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function numberFrmat()
+    {
+        $this->assertEquals('100,001', number_format("100000.698"));
+        $this->assertEquals('100 000,698', number_format("100000.698", 3, ",", " "));
+    }
+
+    /**
+     * @test
+     */
+    public function moneyFormat()
+    {
+        setlocale(LC_MONETARY, "en_US"); //!!
+        $this->assertEquals('100000.70', money_format('%.2n', "100000.698"));
+    }
+
+    /**
+     * @test
+     */
+    public function printF(){
+        $n = 123;
+        $f = 123.45;
+        $s = "A string";
+        /*
+        printf("%d", $n); // prints 123
+        printf("%d", $f); // prints 123
+        // Prints "The string is A string"
+        printf("The string is %s", $s);
+        // Example with precision
+        printf("%3.3f", $f); // prints 123.450 //*/
+    }
+}
---
+++ zend_book/formatting/ParsingFormattedInputTest.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: kirill
+ * Date: 29.12.16
+ * Time: 14:36
+ */
+class ParsingFormattedInputTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function getFormattedInput()
+    {
+        $data = '123 456 789';
+        $format = '%d %d %d';
+        $scan_data = sscanf($data, $format);
+        $this->assertCount(3, $scan_data);
+        $this->assertContains('123', $scan_data);
+        $this->assertContains('456', $scan_data);
+        $this->assertContains('789', $scan_data);
+    }
+}
