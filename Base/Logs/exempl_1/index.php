9c7d4f8371f7256f6c20de7c306bf7a843e5d578
Shitikov Kyrylo <kshy@isddesign.com>
2017-10-26 18:44:02 +0300

    Array

---
+++ Base/Logs/exempl_1/index.php
@@ -0,0 +1,15 @@
+<?php
+require_once 'Logging.php';
+// Logging class initialization
+$log = new Logging();
+
+// set path and name of log file (optional)
+$log->lfile('/tmp/mylog.txt');
+
+// write message to the log file
+$log->lwrite('Test message1');
+$log->lwrite('Test message2');
+$log->lwrite('Test message3');
+
+// close log file
+$log->lclose();
