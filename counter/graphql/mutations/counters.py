e949a775fb81d2b0d9483d17f32776ac53fe140b
Shitikov Kirill <sh.kiruh@gmail.com>
2020-05-11 22:19:49 +0300

    Add logger to counter mutation

--- counter/graphql/mutations/counters.py
+++ counter/graphql/mutations/counters.py
@@ -1,6 +1,9 @@
 import graphene as G
 from counter.models.counters.tables import view_counters
 
+import logging
+log = logging.getLogger(__name__)
+
 
 class Visited(G.Mutation):
     class Arguments:
@@ -13,32 +16,40 @@ class Visited(G.Mutation):
 
     async def mutate(parent, info, url, is_visited):
         app = info.context['request'].app
-        async with app['db'].acquire() as conn:
-            cursor = await conn.execute(
-                view_counters.select().where(view_counters.c.url == url)
+        try:
+            async with app['db'].acquire() as conn:
+                cursor = await conn.execute(
+                    view_counters.select().where(view_counters.c.url == url)
+                )
+                counter = await cursor.fetchone()
+                if counter is None:
+                    views = 1
+                    visitors = 1
+                    stmt = view_counters\
+                        .insert()\
+                        .values(url=url)
+                    await conn.execute(stmt)
+                else:
+                    views = counter.views + 1
+                    visitors = counter.visitors
+                    if not is_visited:
+                        visitors = counter.visitors + 1
+                    stmt = view_counters.update() \
+                        .where(view_counters.c.url == url)\
+                        .values(views=views, visitors=visitors)
+                    await conn.execute(stmt)
+            return Visited(
+                ok=True,
+                views=views,
+                visitors=visitors
             )
-            counter = await cursor.fetchone()
-            if counter is None:
-                views = 1
-                visitors = 1
-                stmt = view_counters\
-                    .insert()\
-                    .values(url=url)
-                await conn.execute(stmt)
-            else:
-                views = counter.views + 1
-                visitors = counter.visitors
-                if not is_visited:
-                    visitors = counter.visitors + 1
-                stmt = view_counters.update() \
-                    .where(view_counters.c.url == url)\
-                    .values(views=views, visitors=visitors)
-                await conn.execute(stmt)
-        return Visited(
-            ok=True,
-            views=views,
-            visitors=visitors
-        )
+        except Exception as err:
+            log.exception("Error view counter service: ", exc_info=err)
+            return Visited(
+                ok=False,
+                views=None,
+                visitors=None
+            )
 
 
 class Mutations(G.ObjectType):
